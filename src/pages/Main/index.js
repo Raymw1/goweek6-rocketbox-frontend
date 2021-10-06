c869f3305f45cf0c241ec4f56409436458307c04
Raymw1 <rayan.rmw@gmail.com>
2021-10-06 16:52:54 -0300

    Main page and functionalities done

---
+++ src/pages/Main/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import api from "../../services/api";
+
+import "./styles.css";
+import logo from "../../assets/logo.svg";
+
+export default class index extends Component {
+  state = {
+    newBox: "",
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await api.post("/boxes", { title: this.state.newBox });
+    console.log(data);
+  };
+
+  handleInputChange = (e) => {
+    this.setState({ newBox: e.target.value });
+  };
+
+  render() {
+    return (
+      <div id="main-container">
+        <form onSubmit={this.handleSubmit}>
+          <img src={logo} alt="RocketBox" />
+          <input
+            type="text"
+            placeholder="Create a box"
+            value={this.state.newBox}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Create</button>
+        </form>
+      </div>
+    );
+  }
+}
